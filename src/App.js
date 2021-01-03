a92b621f892232ec48e0a4f50cb20f5d5a74c695
Dani <dani@Danis-MacBook-Pro.local>
2021-01-03 17:23:35 +0200

    dummy data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import faker from 'faker'
+import {FixedSizeList as theList} from "react-window";
+
+const data = new Array(100).fill().map((value, index) => ({ id: index, title: faker.lorem.words(5), body: faker.lorem.sentences(4) }))
 
 function App() {
-  return (
-    <div className="App">
-      let see what i can learn in my first day of work
-    </div>
-  );
+    return (
+        <div>
+            {data.map(((item) => (
+                <div key={item.id} className="post">
+                    <h3>{item.title} - {item.id}</h3>
+                    <p>{item.body}</p>
+                </div>
+            )))}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/line.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Line(props) {
+    return (
+        <div className="Line">
+            this is line
+        </div>
+    );
+}
+
+export default Line;
